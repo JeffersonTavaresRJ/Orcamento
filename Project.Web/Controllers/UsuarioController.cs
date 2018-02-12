dd1df5a26b45c6939fcdbc5d559635e4cedd3ffa
Jefferson Tavares <jef.tavaresrj@gmail.com>
2018-02-12 12:48:55 -0200

    12-02-2018-Criação do Projeto Web

---
+++ Project.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
