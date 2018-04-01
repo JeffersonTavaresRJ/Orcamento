9e25005ef41079a9bd0f902bbf79ab817b0c4a04
Jefferson Tavares <jef.tavaresrj@gmail.com>
2018-04-01 12:03:04 -0300

    01-04-2018 - Usuario: Criação da Tela de Consulta

--- Project.Entity/Enuns/Status.cs
+++ Project.Entity/Enuns/Status.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Project.Entity.Enuns
 {
     public enum Status
     {
-        Ativo='A',
-        Inativo='I'
+        [Description("Ativo")]
+        Ativo ='A',
+        [Description("Inativo")]
+        Inativo ='I'
     }
 }
