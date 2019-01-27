091b5b9ec133eb7c50c5372a786664245c2f1bc6
Jefferson Tavares <jef.tavaresrj@gmail.com>
2019-01-27 21:03:54 -0200

    27-01-2019: Criação da tela de Grupos de Conta

---
+++ Project.Web/Areas/AreaIndex/Controllers/GrupoController.cs
@@ -0,0 +1,18 @@
+﻿using Project.Entity;
+using Project.Repository.Persistence;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Project.Web.Areas.AreaIndex.Models;
+namespace Project.Web.Areas.AreaIndex.Controllers
+{
+    public class GrupoController : Controller
+    {
+        // GET: AreaIndex/Grupo
+        public ActionResult ManutencaoGrupo()
+        {
+            GrupoPersistence gp = new GrupoPersistence();
+            ViewBag.Grupos = gp.ListarTodos();
+            return View();
+        }
+    }
+}
